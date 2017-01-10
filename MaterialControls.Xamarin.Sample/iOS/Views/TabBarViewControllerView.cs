d56b74644c948a2b766062149ab3a48007867581
carbon-bold <development@boldint.com>
2017-01-10 10:47:58 +0000

    Added new methods in ApiDefenitions.

--- MaterialControls.Xamarin.Sample/iOS/Views/TabBarViewControllerView.cs
+++ MaterialControls.Xamarin.Sample/iOS/Views/TabBarViewControllerView.cs
@@ -6,66 +6,74 @@ using UIKit;
 using MaterialControls;
 using System.Linq;
 using MaterialControls.Xamarin.Sample.Core.ViewModels;
+using System.Threading.Tasks;
 
 namespace MaterialControls.Xamarin.Sample.iOS.Views
 {
-	public partial class TabBarViewControllerView : BaseView<TabBarViewControllerViewModel>
-	{
-		MDTabBarViewController TabBarViewController;
-		TabBarViewControllerDelegate Delegate;
+    public partial class TabBarViewControllerView : BaseView<TabBarViewControllerViewModel>
+    {
+        MDTabBarViewController TabBarViewController;
+        TabBarViewControllerDelegate Delegate;
 
-		public TabBarViewControllerView () : base ("TabBarViewControllerView", null)
-		{
-			Delegate = new TabBarViewControllerDelegate ();
-				TabBarViewController = new MDTabBarViewController (Delegate);
-			TabBarViewController.SetItems (new NSObject [] {
-				new NSString ("FIRST TAB"),
-				new NSString ("SECOND TAB"),
-				new NSString ("THIRD TAB"),
-				new NSString ("FOURTH TAB"),
-				new NSString ("FIFTH TAB"),
-				new NSString ("SIXTH TAB"),
-				new NSString ("SEVENTH TAB"),
-				new NSString ("EIGHTH TAB"),
-				new NSString ("NINTH TAB"),
-				new NSString ("TENTH TAB")
-			});
-		}
+        public TabBarViewControllerView() : base("TabBarViewControllerView", null)
+        {
+            Delegate = new TabBarViewControllerDelegate();
+            TabBarViewController = new MDTabBarViewController(Delegate);
 
-		public override void DidReceiveMemoryWarning ()
-		{
-			// Releases the view if it doesn't have a superview.
-			base.DidReceiveMemoryWarning ();
-			
-			// Release any cached data, images, etc that aren't in use.
-		}
+            TabBarViewController.TabBar.Height = 200;
+            TabBarViewController.TabBar.IndicatorHeight = 1;
+            TabBarViewController.TabBar.ShadowColor = UIColor.Yellow;
+            TabBarViewController.TabBar.ShadowsEnabled = true;
+            TabBarViewController.SetItems(new NSObject[] {
+                new NSString ("FIRST TAB"),
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
-			
-			AddChildViewController (TabBarViewController);
-			UIView controllerView = TabBarViewController.View;
-			View.AddSubview (controllerView);
+            });
 
-			NSDictionary viewsDictionary = new NSDictionary ("TopLayoutGuide", TopLayoutGuide, "BottomLayoutGuide", BottomLayoutGuide, "controllerView", controllerView);
-			View.AddConstraints (NSLayoutConstraint.FromVisualFormat ("V:[TopLayoutGuide]-0-[controllerView]-0-[BottomLayoutGuide]", NSLayoutFormatOptions.DirectionLeadingToTrailing, null, viewsDictionary));
-			View.AddConstraints (NSLayoutConstraint.FromVisualFormat ("H:|-0-[controllerView]-0-|", NSLayoutFormatOptions.DirectionLeadingToTrailing, null, viewsDictionary));
-		}
-	}
+        }
 
-	public class TabBarViewControllerDelegate: MDTabBarViewControllerDelegate
-	{
-		#region implemented abstract members of MDTabBarViewControllerDelegate
+        public override void DidReceiveMemoryWarning()
+        {
+            // Releases the view if it doesn't have a superview.
+            base.DidReceiveMemoryWarning();
 
-		public override UIViewController ViewControllerAtIndex (MDTabBarViewController viewController, nuint index)
-		{
-			TabContentViewController controller = new TabContentViewController("Tab "+ (index + 1));
-			return controller;
-		}
+            // Release any cached data, images, etc that aren't in use.
+        }
 
-		#endregion
-		
-	}
+        public override async void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            AddChildViewController(TabBarViewController);
+            UIView controllerView = TabBarViewController.View;
+            View.AddSubview(controllerView);
+
+            NSDictionary viewsDictionary = new NSDictionary("TopLayoutGuide", TopLayoutGuide, "BottomLayoutGuide", BottomLayoutGuide, "controllerView", controllerView);
+            View.AddConstraints(NSLayoutConstraint.FromVisualFormat("V:[TopLayoutGuide]-0-[controllerView]-0-[BottomLayoutGuide]", NSLayoutFormatOptions.DirectionLeadingToTrailing, null, viewsDictionary));
+            View.AddConstraints(NSLayoutConstraint.FromVisualFormat("H:|-0-[controllerView]-0-|", NSLayoutFormatOptions.DirectionLeadingToTrailing, null, viewsDictionary));
+
+            //await Task.Delay(2000);
+
+        }
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+            TabBarViewController.SetSelectedIndex((nuint)(TabBarViewController.TabBar.NumberOfItems - 1));
+
+        }
+    }
+
+    public class TabBarViewControllerDelegate : MDTabBarViewControllerDelegate
+    {
+        #region implemented abstract members of MDTabBarViewControllerDelegate
+
+        public override UIViewController ViewControllerAtIndex(MDTabBarViewController viewController, nuint index)
+        {
+            TabContentViewController controller = new TabContentViewController("Tab " + (index + 1));
+            return controller;
+        }
+
+        #endregion
+
+    }
 }
 
